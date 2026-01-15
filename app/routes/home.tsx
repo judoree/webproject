9e3f6c28d9baa3132d3f3a268b5530556f861c95
김주현 <judoree9629@gmail.com>
2026-01-15 20:35:24 +0900

    shadcn

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,11 @@
+import { Button } from '~/components/ui/button';
 import type { Route } from './+types/home';
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      Home
+      <Button>Click me</Button>
+    </div>
+  );
 }
